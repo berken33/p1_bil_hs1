b47928368c02d429b40360e08c77174afb81050b
berken33 <148984043+berken33@users.noreply.github.com>
2023-10-25 16:38:09 +0000

    ilk gün

--- main.py
+++ main.py
@@ -1,3 +1,111 @@
-# print('Hello World')
+# ('hello world')
 # print('Merhaba Dünya')
-print('Hello')
+# print('Hello')
+# print('Phyton ile ilk günüm!')
+# print('Merhaba dünya')
+# print('merhaba'+' '+'Şahin')
+# print(input('Merhaba, Adın Nedir?'))
+# print('Merhaba '+ input('Merhaba,Adın Nedir?')+ '!')
+# print(len(input('Merhaba,Adın Nedir?b')))
+# name = input("Merhaba Adın Nedir?")
+# print(name)
+# name = "Şahin"
+# print(name)
+# name = "Taşın"
+# print(name)
+# print(len(input('Merhaba, Adın Nedir?')))
+# isim = input('Merhaba, Adın Nedir?')
+# uzunluk = len(isim)
+# print(uzunluk)
+# sayi1 = input('1nci Sayıyı Giriniz:')
+# sayi2 = input('2nci Sayıyı Giriniz:')
+# sayi3 = sayi1 
+# sayi1 = sayi2
+# sayi2 = sayi3
+# print('sayi1 : ' + sayi1)
+# print('sayi2 : ' + sayi2)0
+# print(len(str(1234567890)))
+# print('Merhaba'[0])
+
+# print(123 + 456)
+# print(123_456_789)
+
+# print(3.12345)
+
+# print(True)
+# print(False)
+
+# isimUzunluk = len(input('Merhaba, Adın ne?ber'))
+# isimUzunluk_Str = str(isimUzunluk)
+# print('İsminde' + isimUzunluk_Str + ' karakter var.')
+
+# f1 = float(1.322)
+# print(type(f1))
+
+# intt = int(232)
+# print(type(intt))
+
+# st = str(123123)
+# print(type(st))
+
+# print(100 + float(15.23))
+# print(str(122) +str (324))
+
+# ikihanelisayi = str(input('iki haneli bir sayı giriniz!'))
+# toplam = int(ikihanelisayi[0]) + int(ikihanelisayi[1])
+# print('toplam: ' + str(toplam))
+
+#print(2 * 4 + 6 / 3 - 2 )
+
+# boy = float(input('boyunuzu giriniz (m): '))
+# kilo = float(input('kilonuzu giriniz (kg):'))
+
+# vki = int(kilo/boy**2)
+# print('vücut kitle indeksiniz: ' + str(vki))
+
+# print(8/3)
+# print(type(8/3))
+# print(8//3)
+# print(type(8//3))
+
+# result = 8/4
+# result /= 2
+# print(result)
+# sayi = 5
+# sayi +=1
+# print(sayi)
+# sayi = sayi+1
+# print(sayi)
+
+# print('Sayı =' +str(sayi))
+# print(f'Sayı = {sayi}')
+#  yas = input('Kaç yaşındasın?')
+# yas_int = int(yas)
+# ay = yas_int * 12
+# hafta = yas_int * 52
+# gun = yas_int * 365
+# print(f'{ay} aylıksın')
+# print(f'{hafta} haftalıksın')
+# print(f'{gun} günlüksün')
+
+# print('Bahşiş hesaplayıcıya hoşgeldiniz!')
+# hesap = float(input('Hesap ne geldi?'))
+# kisiSayisi = int(input('Kaç kişisiniz?'))
+# oran = float(input('Bahşiş oranı ne olsun?'))
+# toplam = hesap + (hesap*oran/100)
+# kisiBasi = toplam /kisiSayisi
+# print(f'Kişibaşı ödemeniz gereken tutar = {kisiBasi} TL')
+
+
+# print('2 kişinin yaşlarının çarpımı kaç?')
+# yas1 = float(input('Kac yasındasınız'))
+# yas2 = float(input('2. kişinin yaşlarını giriniz'))
+# sayi3 = int(yas1*yas2)
+# print('çarpımları ' + str(sayi3))
+
+print('Girilen 3 sayının ortalaması')
+sayi1 = float(input('1.sayıyı giriniz '))
+sayi2 = float(input('2. sayıyı giriniz '))
+sayi3 = float(input('3.sayıyı giriniz '))
+ortalama = int((sayi1 + sayi2 + sayi3)/3)
+print('Sayıların ortalaması ' + str(ortalama))
